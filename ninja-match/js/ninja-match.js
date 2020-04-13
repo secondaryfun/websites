2a16262e7ec7ec013745366cc13aad7a117e4ece
secondaryfun <c.birkenhagen@gmail.com>
2020-04-13 12:40:43 -0400

    Started Card Randomizer

--- ninja-match/js/ninja-match.js
+++ ninja-match/js/ninja-match.js
@@ -7,7 +7,7 @@ readyContainer = document.getElementById("readyContainer");
 
 //      --==Objects==--
 
-function card(loc, fac, col) {
+function Card(loc, fac, col) {
     this.location = loc;
     this.face = fac;
     this.color = col;
@@ -32,14 +32,49 @@ matchingGame = {
         boardSize = diff;
         createBoard(boardSize);
         readyContainer.style.display = "none";
-    }
-
+    },
+    // method() - fills cardsInPlay with pairs of qty(diff/2)
+    pickPairs : function() {
+        //fill array from deck.faces
+        let faces = []
+        for (i=0;i<cardDeck.faces.length;i++) {
+            faces.push(cardDeck.faces[i]);
+        }
+        //choose diff/2 faces
+        let numPairs = Math.floor(diff/2);
+        let pairs = [];
+        for (i=0;i<numPairs;i++) {
+            randomIndex = Math.floor(Math.random() * faces.length);
+            pairs.push(faces[randomIndex]);
+            faces.splice(randomIndex, 1);
+        }
+        //create diff.length cards in cardsInPlay
+        for (i=0;i<diff.length;i++) {
+            for (p=0;p<pairs.length;p++) {
+                for (c=0;c<cardDeck.colors.length;c++)
+                cardsInPlay[i] = new card(null,pairs[p],cardDeck.colors[c])
+            }
+        }
+        return cardsInPlay;
+    },
     //  method() - fills cardsInPlay with pairs of qty(diff/2) and randomizes their locations.
     dealCards   : function() {
-        null
+        dealCards(cardsInPlay, diff)
     }   
     
-}
+}//--------------END GAME OBJECT--------------
+
+//  ------=====Current Test=====------
+
+
+x = startCard.addEventListener("click",function() {
+    console.log("card clicked");
+    // matchingGame.createBoard();
+    test = matchingGame.pickPairs();
+    console.log(JSON.stringify(test));
+})
+//----------------------------------------
+
 
 
 //      -=Global Functions=-
@@ -54,11 +89,31 @@ function createBoard(numCards) {
         board.appendChild(newDiv);
     }
 }
+function dealCards(cardsInPlay, numCards) {
+    let pairs = [], colors = [];
 
+    for (i=0;i<numCards;i++) {
+        random = Math.floor(Math.random() * numCards);
+
+    }
+}
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
 
 //      ---==EVENT LISTENERS==---
-x = startCard.addEventListener("click",function() {
-    console.log("card clicked");
-    matchingGame.createBoard();
-
-})
